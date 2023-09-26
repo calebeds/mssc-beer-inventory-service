30a1d49b2a375a8971b17ad4dbdbee8e20ca4170
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-09-26 17:09:38 -0300

    update parent bom

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-17T13:53:03-0300",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
+    date = "2023-09-26T17:07:57-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
 public class BeerInventoryMapperImpl implements BeerInventoryMapper {
