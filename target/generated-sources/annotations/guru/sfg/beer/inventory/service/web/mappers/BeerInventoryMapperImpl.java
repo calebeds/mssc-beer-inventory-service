be1e6e1647da1d51d8ae5f8b23a31cc7056c0655
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-10-01 15:27:20 -0300

    add allocation service

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-27T10:29:24-0300",
+    date = "2023-10-01T15:24:15-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.20.1 (Ubuntu)"
 )
 @Component
